d03bc8846f053b05007a87eaa4140370bca6e528
MaryaMalajku <malajkumasha@gmail.com>
2020-12-14 14:07:39 +0200

    added description

--- src/components/ItemCar/index.js
+++ src/components/ItemCar/index.js
@@ -34,8 +34,8 @@ render() {
     <div className={styles.carItem}>
         <img src={imageSrc} alt="car.png" className={styles.carItem__picture}/>
         <h3 className = {styles.title}>{title}</h3>
-        <p className="description">Going by the results of this year’s JD Power Initial Quality Survey, though, that’s all wrong. “It’s almost the complete reverse,” says Dave Sargent, who oversees Power's vehicle quality research.</p>
-        
+        {/* <p className={styles.description}>Going by the results of this year’s JD Power Initial Quality Survey, though, that’s all wrong. “It’s almost the complete reverse,” says Dave Sargent, who oversees Power's vehicle quality research.</p> */}
+      
         <div style={{display: 'flex', justifyContent: 'space-around', width: '100%'}}>
           
           <button type="button" className={this.state.liked ? styles.carItem__liked : styles.carItem__to_like } onClick={()=> {this.likeThis()}}>
@@ -51,7 +51,8 @@ render() {
         </div>
 
         { this.state.showDes ?
-            <p className="description">{description}</p> : null
+            <p className="description">Going by the results of this year’s JD Power Initial Quality Survey, though, that’s all wrong. “It’s almost the complete reverse,” says Dave Sargent, who oversees Power's vehicle quality research.
+            </p> : null
             }
 
     </div>

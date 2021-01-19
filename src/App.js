769ed98265ab056d5c50f4ea174e6fa83fe731dc
MaryaMalajku <malajkumasha@gmail.com>
2021-01-19 18:12:45 +0200

    dz 24

--- src/App.js
+++ src/App.js
@@ -9,19 +9,39 @@ class App extends React.Component {
     super();
 
     this.state = {
-      car: carData,
+      apiData: [],
+      error: 0,
     };
+
+    console.log('constructor');
 
   }
 
   deleteCard = (element) => {
-    const resumeCars = this.state.car.filter( item => item.id !== element.id );
+    const resumeCars = this.state.apiData.filter( item => item.id !== element.id );
     this.setState({
-      car: resumeCars
+      apiData: resumeCars
     });
   }
 
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://api.themoviedb.org/3/discover/tv?api_key=4298cee904c2bf07d4c1a191530ce2bc')
+    .then((response) => {
+      this.setState({
+        error: response.status
+      })
+      return response.json()
+    })
+    .then((data) => {
+      this.setState({
+        apiData: data.result,
+      })
+    })
+  }
+
   render() {
+    console.log(this.state.error)
     return (
       <div className="App">
         <div className='cards'>
